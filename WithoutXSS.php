7a89c30bb0ee32e6ec28c79c8972a05ad27fff5e
Marina2927 <32886882+Marina2927@users.noreply.github.com>
2020-05-13 16:56:19 +1000

    xss

---
+++ WithoutXSS.php
@@ -0,0 +1,29 @@
+<meta charset="UTF-8">
+<meta name="viewport"
+      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Without XSS</title>
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+</style>
+<?php
+$form = "
+<div style='display: flex; justify-content: center; align-items: center; flex-direction: column;width: 100vw; height: 100vh'>
+<div style=''>
+    <form>
+        <input type='text' placeholder='Your name' name='name'/>
+        <input type='submit'>
+    </form>
+";
+echo $form;
+$name = $_GET['name'];
+if ($name == "") {
+    $name = "guest";
+}
+echo "<p>Hello " . htmlspecialchars($name) . "</p></div></div>";
+
+
